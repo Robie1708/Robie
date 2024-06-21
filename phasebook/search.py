6b18b95f039fba2a58e7c05b8a51be5c6b6a6538
Robie1708 <lacaocao0908@gmail.com>
2024-06-21 17:08:52 +0800

    Update search.py

--- phasebook/search.py
+++ phasebook/search.py
@@ -2,34 +2,30 @@ from flask import Blueprint, request
 
 from .data.search_data import USERS
 
-# Define a Blueprint for search-related routes
+
 bp = Blueprint("search", __name__, url_prefix="/search")
 
 @bp.route("")
 def search():
-    return {"users": search_users(request.args.to_dict())}, 200
+    return {"users": search_users(request.args)}, 200
 
 def search_users(args):
     """Search users database
 
     Parameters:
-        args: a dictionary containing the following search parameters:
-            id: string
-            name: string
-            age: string
-            occupation: string
+        args: a MultiDict containing the search parameters from request.args
 
     Returns:
         a list of users that match the search parameters
     """
 
-    # Extract search parameters from the args dictionary
+    
     search_id = args.get('id')
     search_name = args.get('name', '').lower()
-    search_age = args.get('age')
+    search_age = args.get('Age')
     search_occupation = args.get('occupation', '').lower()
 
-    # Convert search_age to an integer if it exists
+   
     if search_age is not None:
         search_age = int(search_age)
 
@@ -40,11 +36,11 @@ def search_users(args):
             matched_users.append(user)
             continue
 
-        if search_name and search_name in user['name'].lower():
+        if search_age is not None and search_age - 1 <= user['age'] <= search_age + 1:
             matched_users.append(user)
             continue
 
-        if search_age and search_age - 1 <= user['age'] <= search_age + 1:
+        if search_name and search_name in user['name'].lower():
             matched_users.append(user)
             continue
 
@@ -52,7 +48,7 @@ def search_users(args):
             matched_users.append(user)
             continue
 
-    # Ensure unique results
-    matched_users = {user['id']: user for user in matched_users}.values()
+   
+    matched_users = list({user['id']: user for user in matched_users}.values())
 
-    return list(matched_users)
+    return matched_users
